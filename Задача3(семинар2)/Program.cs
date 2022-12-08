73acc1b2288713f6ad336bf67e4651320aeacead
koord1nator <119070101+koord1nator@users.noreply.github.com>
2022-12-08 19:51:48 +0300

    Add files via upload

---
+++ Задача3(семинар2)/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    {
+        if((num % 7 == 0) && (num % 23 == 0))
+        {
+            Console.WriteLine("Кратно одновременно 7 и 23: ");
+        }
+
+        else
+
+        {
+            Console.WriteLine("Не кратно одновременно 7 и 23: "); 
+        }
+    }
+}
